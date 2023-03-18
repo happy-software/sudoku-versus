23eb2d7acf484dfd2f08fc71ae0826f58c7cee8a
Hebron George <hebrontgeorge@gmail.com>
2023-03-18 17:36:40 -0400

    WIP trying to get join match working; right now if the board gets loaded up, none of the stimulus actions trigger for some reason but if you load the index page (ie load the board fresh) then they work so something's going on but idk what yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,32 +1,20 @@
 class HomeController < ApplicationController
   def index
     @board = SudokuBuilder.create.hard.to_a.flatten
-    # @solved = [[[2, 5, 7], [9, 1, 6], [3, 8, 4]],
-    #            [[4, 8, 1], [3, 5, 2], [9, 6, 7]],
-    #            [[3, 6, 9], [4, 7, 8], [5, 1, 2]],
-    #            [[1, 2, 3], [8, 9, 5], [7, 4, 6]],
-    #            [[5, 7, 8], [6, 3, 4], [1, 2, 9]],
-    #            [[9, 4, 6], [1, 2, 7], [8, 5, 3]],
-    #            [[6, 9, 4], [7, 8, 1], [2, 3, 5]],
-    #            [[8, 3, 2], [5, 4, 9], [6, 7, 1]],
-    #            [[7, 1, 5], [2, 6, 3], [4, 9, 8]]].flatten
-    #
-    # @board = [[[2, 5, nil], [nil, nil, nil], [nil, 8, nil]],
-    #           [[4, 8, 1], [3, 5, nil], [nil, 6, 7]],
-    #           [[3, nil, nil], [4, 7, nil], [5, nil, nil]],
-    #           [[nil, 2, nil], [nil, 9, nil], [nil, 4, nil]],
-    #           [[5, 7, 8], [6, nil, nil], [1, nil, 9]],
-    #           [[nil, nil, 6], [nil, 2, 7], [8, nil, nil]],
-    #           [[nil, 9, nil], [nil, nil, 1], [nil, nil, 5]],
-    #           [[nil, nil, 2], [5, 4, 9], [nil, 7, nil]],
-    #           [[nil, 1, 5], [2, 6, nil], [nil, nil, nil]]].flatten
   end
 
   def new
+    @player_1_start = true
+    @player_2_start = false
   end
 
   def create_challenge
-    @user_name = (session[:user_name] || params[:userNameInput]).titleize
+    session[:user_name] ||= params[:user_name_input].titleize
+    @user_name        = session[:user_name]
+    @difficulty_level = params[:difficulty_level]
+    match = create_new_match(difficulty: @difficulty_level.to_s, player_1_name: @user_name)
+    @challenge_url = join_match_url(match_key: match.match_key)
+
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
@@ -38,5 +26,58 @@ class HomeController < ApplicationController
 
   def waiting_for_challenger
     @challenge_creator_name = params[:match_uuid] # TODO: Update this stubbed value
+  end
+
+  def join_match
+    @player_1_start = false
+    @player_2_start = true
+
+    match = Match.find_by!(match_key: params[:match_key])
+    @player_1_name = match.player_1_name
+    @match_key     = match.match_key
+    @difficulty_level = match.difficulty_level
+    render 'new'
+  end
+
+  def accept_challenge
+    session[:user_name] ||= params[:user_name_input].titleize
+    @user_name = session[:user_name]
+
+    match = Match.find_by!(match_key: params[:match_key])
+    match.player_2_name = @user_name
+    match.save!
+
+    @board = match.starting_board
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace('new_challenge_container', partial: 'home/board')
+        ]
+      end
+      # format.turbo_stream { render turbo_stream: turbo_stream.replace('players_form', partial: 'new_player') }
+
+    end
+  end
+
+  private
+
+  def create_new_match(difficulty:, player_1_name:)
+    if difficulty.to_s.downcase == 'easy'
+      game = SudokuBuilder.create.easy
+    elsif difficulty.to_s.downcase == 'medium'
+      game = SudokuBuilder.create.medium
+    elsif difficulty.to_s.downcase == 'hard'
+      game = SudokuBuilder.create.hard
+    elsif difficulty.to_s.downcase == 'very_hard'
+      game = SudokuBuilder.create.poke(55)
+    else
+      ArgumentError.new("Given difficulty level: (#{difficulty}) is not recognized! Possible Options: (Easy, Medium, Hard, Very Hard)")
+    end
+
+    starting_board = game.to_a.flatten
+    solution       = game.solve.to_a.flatten
+    match_key      = SecureRandom.uuid
+    Match.create!(starting_board: starting_board, solution: solution, match_key: match_key, player_1_name: player_1_name, difficulty_level: difficulty)
   end
 end
