446351d8ce1972a2f751a21ea4ce363d7940c06f
Hebron George <hebrontgeorge@gmail.com>
2023-03-19 10:52:09 -0400

    Include the numpad digits in onkeydown check

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -7,7 +7,8 @@ export default class extends Controller {
     let that = this;
     document.onkeydown = function (e) {
       // TODO: This can probably be broken up into methods on the CellController and delegated over using [Outlets](https://stimulus.hotwired.dev/reference/outlets)
-      if (!['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Backspace'].includes(e.code)) {
+      if (!['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Backspace',
+        'Numpad1', 'Numpad2', 'Numpad3', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad7', 'Numpad8', 'Numpad9'].includes(e.code)) {
         return;
       }
       let selectedCell = that.cellTargets.find(cell => cell.classList.contains("selected"))
