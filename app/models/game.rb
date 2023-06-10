4f02bfd407c2e5cacebb23d583399d3ef2719bdb
Hebron George <hebrontgeorge@gmail.com>
2023-06-09 22:57:12 -0400

    Display Game Over Stats

--- app/models/game.rb
+++ app/models/game.rb
@@ -24,4 +24,51 @@ class Game < ApplicationRecord
 
     board.none?(nil)
   end
+
+  def stats
+    self.submissions   ||= []
+    return empty_stats if self.submissions.empty?
+
+    sorted_submissions = self.submissions.sort_by { |s| s.fetch('timestamp').to_datetime }
+    first_move_time    = sorted_submissions.first.fetch('timestamp').to_datetime
+    last_move_time     = sorted_submissions.last.fetch('timestamp').to_datetime
+    accuracy           = (self.submissions.select { |s| s.fetch('is_correct') }.count.to_f / self.submissions.count.to_f) * 100
+
+    {
+      played_move_count: self.submissions.count,
+      accuracy:          accuracy,
+      player_number:     self.player_number,
+      player_name:       self.player_name,
+      start_time:        first_move_time,
+      end_time:          last_move_time,
+      total_time:        humanize(last_move_time.to_i - first_move_time.to_i),
+      completed_board:   self.game_over?,
+    }
+  end
+
+  private
+
+  # Copied from StackOverflow post: https://stackoverflow.com/a/4136485
+  def humanize(secs)
+    [[60, :seconds], [60, :minutes], [24, :hours], [Float::INFINITY, :days]].map{ |count, name|
+      if secs > 0
+        secs, n = secs.divmod(count)
+
+        "#{n.to_i} #{name}" unless n.to_i==0
+      end
+    }.compact.reverse.join(' ')
+  end
+
+  def empty_stats
+    {
+      played_move_count: 0,
+      accuracy:          0,
+      player_name:       self.player_name,
+      player_number:     self.player_number,
+      start_time:        nil,
+      end_time:          nil,
+      total_time:        "No moves played!",
+      completed_board:   false,
+    }
+  end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -8,4 +8,21 @@ class Match < ApplicationRecord
   def player_2_game
     games.where(player_number: :player_2, player_name: player_2_name).first
   end
+
+  def game_over_stats
+    player_1_stats = player_1_game.stats
+    player_2_stats = player_2_game.stats
+
+    winner = [player_1_stats, player_2_stats].find { |stats| stats.fetch(:completed_board) }&.fetch(:player_number) || "N/A"
+
+    {
+      winner:            winner,
+      player_1_name:     player_1_stats.fetch(:player_name),
+      player_2_name:     player_2_stats.fetch(:player_name),
+      player_1_time:     player_1_stats.fetch(:total_time),
+      player_2_time:     player_2_stats.fetch(:total_time),
+      player_1_accuracy: player_1_stats.fetch(:accuracy).round(2),
+      player_2_accuracy: player_2_stats.fetch(:accuracy).round(2),
+    }
+  end
 end
