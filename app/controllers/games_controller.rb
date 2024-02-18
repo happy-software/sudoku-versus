5ca0420d74e628a32f2cd23580f2b38a7f644782
Hebron George <hebrontgeorge@gmail.com>
2024-02-18 15:19:18 -0500

    Render game stats if the match is over when going to the game url directly

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,17 @@
 class GamesController < ApplicationController
   def show
-    @game      = Game.find_by_uuid!(params[:id])
-    @match     = @game.match
+    @game  = Game.find_by_uuid!(params[:id])
+    @match = @game.match
+
+    if @match.match_ended?
+      stats = @match.game_over_stats
+      # TODO: Fix this view.
+      #   In the normal flow, when a game is over, the stats get swapped in for the #gameContainer div
+      #   but since the stats partial is getting loaded in by itself here, the layout is missing and
+      #   the view looks barren (not centered, missing fonts, bootstrap buttons, etc)
+      render partial: 'game/stats', locals: { final_stats: stats }
+    end
+
     @board     = @match.starting_board
     @match_key = @match.match_key
     @game_uuid = @game.uuid
