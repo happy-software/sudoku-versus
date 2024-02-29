dd67c76ad795e349fcd5030225736ca9bba40ad3
Hebron George <hebrontgeorge@gmail.com>
2024-02-28 21:17:23 -0500

    Remove unncessary return

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,19 +6,19 @@ Rack::Attack.blocklist('block common entry points and vulnerabilities') do |req|
   if req.path =~ %r{/wp-admin|/admin|/login|/admin/.*}
     # Return true to block the request
     puts "Blocking request searching for common entry points: #{req.path}"
-    return true
+    true
   end
 
   # Block requests attempting to access sensitive files
   if req.path =~ /\.(git|env)/
     puts "Blocking request attempting to access sensitive files: #{req.path}"
-    return true
+    true
   end
 
   # Block requests with known exploit attempts (e.g., SQL injection, XSS)
   # Modify the conditions according to your specific detection needs
   if req.path =~ /(SELECT|UNION|INSERT|DELETE|UPDATE|CREATE|DROP|ALTER)/i || req.params.any? { |k, v| v =~ /(SELECT|UNION|INSERT|DELETE|UPDATE|CREATE|DROP|ALTER)/i }
     puts "Blocking request attempting code injection attacks: #{req.path}"
-    return true
+    true
   end
 end
