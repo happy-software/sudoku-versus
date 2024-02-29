e84b3bf9bea0a89cd692a3846624764c91af720e
Hebron George <hebrontgeorge@gmail.com>
2024-02-28 22:02:00 -0500

    Print ip address in log

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -5,7 +5,7 @@ Rack::Attack.blocklist('block common entry points and vulnerabilities') do |req|
   # Block requests targeting common entry points
   if req.path =~ %r{/wp-admin|/admin|/login|/admin/.*}
     # Return true to block the request
-    puts "Blocking request searching for common entry points: #{req.path}"
+    puts "[#{req.ip}] Blocking request searching for common entry points: #{req.path}"
     true
   end
 end
@@ -13,7 +13,7 @@ end
 Rack::Attack.blocklist("sensitive files scanning") do |req|
   # Block requests attempting to access sensitive files
   if req.path =~ /\.(git|env)/
-    puts "Blocking request attempting to access sensitive files: #{req.path}"
+    puts "[#{req.ip}] Blocking request attempting to access sensitive files: #{req.path}"
     true
   end
 end
@@ -22,7 +22,7 @@ Rack::Attack.blocklist("code injection attempts") do |req|
   # Block requests with known exploit attempts (e.g., SQL injection, XSS)
   # Modify the conditions according to your specific detection needs
   if req.path =~ /(SELECT|UNION|INSERT|DELETE|UPDATE|CREATE|DROP|ALTER)/i || req.params.any? { |k, v| v =~ /(SELECT|UNION|INSERT|DELETE|UPDATE|CREATE|DROP|ALTER)/i }
-    puts "Blocking request attempting code injection attacks: #{req.path}"
+    puts "[#{req.ip}] Blocking request attempting code injection attacks: #{req.path}"
     true
   end
 end
