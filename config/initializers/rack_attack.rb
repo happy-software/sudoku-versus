e59c6ef77fa3f87515b3f9feeba91e68de5a146e
Hebron George <hebrontgeorge@gmail.com>
2024-02-28 21:55:37 -0500

    Split up blocklist handling

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,19 +8,23 @@ Rack::Attack.blocklist('block common entry points and vulnerabilities') do |req|
     puts "Blocking request searching for common entry points: #{req.path}"
     true
   end
+end
 
-  # # Block requests attempting to access sensitive files
-  # if req.path =~ /\.(git|env)/
-  #   puts "Blocking request attempting to access sensitive files: #{req.path}"
-  #   true
-  # end
-  #
-  # # Block requests with known exploit attempts (e.g., SQL injection, XSS)
-  # # Modify the conditions according to your specific detection needs
-  # if req.path =~ /(SELECT|UNION|INSERT|DELETE|UPDATE|CREATE|DROP|ALTER)/i || req.params.any? { |k, v| v =~ /(SELECT|UNION|INSERT|DELETE|UPDATE|CREATE|DROP|ALTER)/i }
-  #   puts "Blocking request attempting code injection attacks: #{req.path}"
-  #   true
-  # end
+Rack::Attack.blocklist("sensitive files scanning") do |req|
+  # Block requests attempting to access sensitive files
+  if req.path =~ /\.(git|env)/
+    puts "Blocking request attempting to access sensitive files: #{req.path}"
+    true
+  end
+end
+
+Rack::Attack.blocklist("code injection attempts") do |req|
+  # Block requests with known exploit attempts (e.g., SQL injection, XSS)
+  # Modify the conditions according to your specific detection needs
+  if req.path =~ /(SELECT|UNION|INSERT|DELETE|UPDATE|CREATE|DROP|ALTER)/i || req.params.any? { |k, v| v =~ /(SELECT|UNION|INSERT|DELETE|UPDATE|CREATE|DROP|ALTER)/i }
+    puts "Blocking request attempting code injection attacks: #{req.path}"
+    true
+  end
 end
 
 Rack::Attack.blocklisted_response = lambda do |_env|
