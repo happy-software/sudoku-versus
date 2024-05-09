9acb6d95ef5e98f68148aa88795db542c8de1a7a
Hebron George <hebrontgeorge@gmail.com>
2024-05-09 00:27:18 -0400

    Add todos for later

--- app/models/rematch_request.rb
+++ app/models/rematch_request.rb
@@ -3,6 +3,8 @@ class RematchRequest < ApplicationRecord
   belongs_to :challengee_game, class_name: "Game"
   belongs_to :match
 
+  # TODO: Add reject! and rejected? helpers
+
   def accept!
     self.accepted_at = DateTime.now
     self.save!
