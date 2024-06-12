3ff7aa54d40f16263163f8fc46414093a5e0808a
Hebron George <hebrontgeorge@gmail.com>
2024-06-11 23:24:52 -0400

    Create a recent games history table to show the most recent 10 games

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,8 @@
 class Match < ApplicationRecord
   has_many :games
 
+  scope :recently_ended, -> { where.not(ended_at: nil).order(ended_at: :desc) }
+
   def match_started?
     self.started_at?
   end
