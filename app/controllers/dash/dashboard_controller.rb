7bfb5010a50b86920f6b6e96baa26b1ff2e0a8b8
Hebron George <hebrontgeorge@gmail.com>
2026-09-07 14:41:06 -0400

    Add completion rates across time ranges for dashboard

--- app/controllers/dash/dashboard_controller.rb
+++ app/controllers/dash/dashboard_controller.rb
@@ -6,9 +6,20 @@ module Dash
       @completed_matches = Match.where.not(ended_at: nil).count
       @completion_rate   = @total_matches.zero? ? 0 : (@completed_matches.to_f / @total_matches * 100).round(1)
 
-      @matches_24h = Match.where("created_at >= ?", 24.hours.ago).count
-      @matches_7d  = Match.where("created_at >= ?", 7.days.ago).count
-      @matches_30d = Match.where("created_at >= ?", 30.days.ago).count
+      @matches_24h = Match.where("created_at >= ?", 24.hours.ago)
+      @matches_7d  = Match.where("created_at >= ?", 7.days.ago)
+      @matches_30d = Match.where("created_at >= ?", 30.days.ago)
+      @matches_6m  = Match.where("created_at >= ?", 6.months.ago)
+
+      @matches_24h_count = @matches_24h.count
+      @matches_7d_count  = @matches_7d.count
+      @matches_30d_count = @matches_30d.count
+      @matches_6m_count = @matches_6m.count
+
+      @matches_24h_completed = @matches_24h.where.not(ended_at: nil).count
+      @matches_7d_completed  = @matches_7d.where.not(ended_at: nil).count
+      @matches_30d_completed = @matches_30d.where.not(ended_at: nil).count
+      @matches_6m_completed = @matches_6m.where.not(ended_at: nil).count
 
       @unique_visitors = Ahoy::Visit.distinct.count(:visitor_token)
       @total_visits    = Ahoy::Visit.count
